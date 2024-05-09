bf6c2f7730e0f531645c409d57d7de750b8465e3
Péter Binder <pbinder.hu@gmail.com>
2024-05-09 14:08:48 +0200

    Initial commit

---
+++ src/main/java/hu/gde/gderunneraf/GdeRunnerAfApplication.java
@@ -0,0 +1,13 @@
+package hu.gde.gderunneraf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdeRunnerAfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GdeRunnerAfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/gde/gderunneraf/GdeRunnerAfApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.gde.gderunneraf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdeRunnerAfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
